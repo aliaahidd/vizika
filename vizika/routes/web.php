59011178f97afe14cda74e20d19787725de04b75
Alia Hidayah <aliahidayah00@gmail.com>
2023-03-21 00:41:01 +0800

    setup environment

---
+++ vizika/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//do not want to display welcome page 
+Route::get('/', function () {
+    if ($user = Auth::user()) {
+        //if login
+        return redirect('/dashboard');
+    } else {
+        //if not login
+        return redirect('login');
+    }
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
