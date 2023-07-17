f840e95069d962e099f6379b8b05235c0f393d15
Alia Hidayah <56750777+aliaahidd@users.noreply.github.com>
2023-07-17 14:50:36 +0800

    Add files via upload

--- vizika.sql
+++ vizika.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 03, 2023 at 06:28 AM
+-- Generation Time: Jul 17, 2023 at 08:17 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -24,6 +24,232 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `appointmentinfo`
+--
+
+CREATE TABLE `appointmentinfo` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `staffID` bigint(20) NOT NULL,
+  `contVisitID` bigint(20) NOT NULL,
+  `appointmentPurpose` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `appointmentAgenda` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `appointmentDate` date NOT NULL,
+  `appointmentTime` time NOT NULL,
+  `bringVehicle` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `bringLaptop` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `appointmentStatus` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `biometricinfo`
+--
+
+CREATE TABLE `biometricinfo` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `userID` bigint(20) NOT NULL,
+  `facialRecognition` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blacklistvisitor`
+--
+
+CREATE TABLE `blacklistvisitor` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `userID` bigint(20) NOT NULL,
+  `blacklistReason` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `briefingsession`
+--
+
+CREATE TABLE `briefingsession` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `briefingID` bigint(20) NOT NULL,
+  `contractorID` bigint(20) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `companyinfo`
+--
+
+CREATE TABLE `companyinfo` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `companyName` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `companyEmail` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `companyPhoneNo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `companyAddress` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contractorinfo`
+--
+
+CREATE TABLE `contractorinfo` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `userID` bigint(20) NOT NULL,
+  `companyID` bigint(20) NOT NULL,
+  `employeeNo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phoneNo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `passExpiryDate` date NOT NULL,
+  `birthDate` date NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `validityPassPhoto` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `laptopinfo`
+--
+
+CREATE TABLE `laptopinfo` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `appointmentID` bigint(20) NOT NULL,
+  `laptopBrand` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `laptopModel` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `laptopColor` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `laptopSerialNo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(144, '2014_10_12_000000_create_users_table', 1),
+(145, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+(146, '2014_10_12_100000_create_password_resets_table', 1),
+(147, '2019_08_19_000000_create_failed_jobs_table', 1),
+(148, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(149, '2023_03_24_134814_create_contractorinfo_table', 1),
+(150, '2023_03_26_033852_create_appointmentinfo_table', 1),
+(151, '2023_03_29_133443_create_visitorinfo_table', 1),
+(152, '2023_03_29_161140_create_blacklistvisitor_table', 1),
+(153, '2023_03_29_162020_create_briefingsession_table', 1),
+(154, '2023_03_29_163029_create_safetybriefinginfo_table', 1),
+(155, '2023_03_29_163506_create_visitrecord_table', 1),
+(156, '2023_03_31_072517_create_companyinfo_table', 1),
+(157, '2023_05_31_161858_create_biometricinfo_table', 1),
+(158, '2023_07_09_145916_create_laptopinfo_table', 1),
+(159, '2023_07_13_153223_create_vehicleinfo_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `safetybriefinginfo`
+--
+
+CREATE TABLE `safetybriefinginfo` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `briefingDate` date NOT NULL,
+  `briefingTimeStart` time NOT NULL,
+  `briefingTimeEnd` time NOT NULL,
+  `maxParticipant` bigint(20) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -34,29 +260,151 @@ CREATE TABLE `users` (
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `category` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `recommendedBy` bigint(20) DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `users`
+-- Table structure for table `vehicleinfo`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `category`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Nurin Azyyati', 'theocloud1479@gmail.com', NULL, '$2y$10$zw6n9d0FuxAUIMiRtN6Zm.NQDo3lGuHMi3kI5nBwne7l3g.fxMPpi', 'SHEQ Officer', NULL, '2023-06-14 18:22:15', '2023-06-14 18:22:15'),
-(2, 'Nurain Aleeya', 'nurainaleeyacz@gmail.com', NULL, '$2y$10$.FYQwNcCW4Po3w6KJEf2s.aMZ4RXL0J1wp6uZeei8.8upxpZ0hb3m', 'Staff', NULL, '2023-06-14 18:41:25', '2023-06-14 18:41:25'),
-(3, 'Maisarah Faisal', 'maisarahfaisal75@gmail.com', NULL, '$2y$10$nTK7MHnSiPDq3NRBUbCotuLdpYXkuaiAXgvjiLFBhpFkT29geq4Mi', 'SHEQ Guard', NULL, '2023-06-14 18:42:16', '2023-06-14 18:42:16'),
-(4, 'Nur Alia Hidayah', 'aliahidayah00@gmail.com', NULL, '$2y$10$jfau1IdzNUkOyo3574eWpOOu1RHEPUfWJ2XQ4UCIfO3S2YqSMirhK', 'Contractor', NULL, NULL, NULL),
-(5, 'Farra Alia', 'farraalia280@gmail.com', NULL, '$2y$10$tpIyRZhYVdhX798srYujbu7qfjEHgWSjzhV/dz3BsfcJ8dJhVea6.', 'Visitor', NULL, NULL, '2023-06-17 02:05:30'),
-(6, 'Nur Afiqah', 'fiqahimnida@gmail.com', NULL, '$2y$10$nUx0OfMPg18ZUbvfOW2zdu19NHEg5lPCb/HFRHE.wEZlT5bPLf1Dy', 'Visitor', NULL, NULL, NULL),
-(7, 'Nurul Huda', 'hudaramli01@gmail.com', NULL, '$2y$10$StXmxZ1.5RsEVPB0oAJMteajj5SoDGYKy3VzjmxieS7w5iTBz0ESi', 'Visitor', NULL, NULL, NULL),
-(8, 'Nursyahkina Othman', 'kino@gmail.com', NULL, '$2y$10$9JGINh8GzOZ9JcrLGGznG.Lxr6E1QJFtwZ.abkgBbJXa9iprHxZtO', 'Staff', NULL, '2023-06-18 02:46:29', '2023-06-18 02:46:29'),
-(9, 'Amira Natasha', 'nnatasha0607@gmail.com', NULL, '$2y$10$OrfE3bQeMwMuEdusZ7IpxO5gt3v6Oq7mIhM1qgbBl3pNwsJ7LEqMy', 'Visitor', NULL, NULL, NULL);
+CREATE TABLE `vehicleinfo` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `appointmentID` bigint(20) NOT NULL,
+  `vehicleType` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `vehicleBrand` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `vehicleColor` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `vehicleRegNo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visitorinfo`
+--
+
+CREATE TABLE `visitorinfo` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `userID` bigint(20) NOT NULL,
+  `companyID` bigint(20) NOT NULL,
+  `employeeNo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `occupation` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phoneNo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `birthDate` date NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visitrecord`
+--
+
+CREATE TABLE `visitrecord` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `appointmentID` bigint(20) NOT NULL,
+  `passNo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `checkInDate` date NOT NULL,
+  `checkInTime` time NOT NULL,
+  `checkOutDate` date DEFAULT NULL,
+  `checkOutTime` time DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `appointmentinfo`
+--
+ALTER TABLE `appointmentinfo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `biometricinfo`
+--
+ALTER TABLE `biometricinfo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `blacklistvisitor`
+--
+ALTER TABLE `blacklistvisitor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `briefingsession`
+--
+ALTER TABLE `briefingsession`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `companyinfo`
+--
+ALTER TABLE `companyinfo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contractorinfo`
+--
+ALTER TABLE `contractorinfo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `laptopinfo`
+--
+ALTER TABLE `laptopinfo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `safetybriefinginfo`
+--
+ALTER TABLE `safetybriefinginfo`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -66,14 +414,116 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `users_email_unique` (`email`);
 
 --
+-- Indexes for table `vehicleinfo`
+--
+ALTER TABLE `vehicleinfo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `visitorinfo`
+--
+ALTER TABLE `visitorinfo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `visitrecord`
+--
+ALTER TABLE `visitrecord`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `appointmentinfo`
+--
+ALTER TABLE `appointmentinfo`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `biometricinfo`
+--
+ALTER TABLE `biometricinfo`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `blacklistvisitor`
+--
+ALTER TABLE `blacklistvisitor`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `briefingsession`
+--
+ALTER TABLE `briefingsession`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `companyinfo`
+--
+ALTER TABLE `companyinfo`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `contractorinfo`
+--
+ALTER TABLE `contractorinfo`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `laptopinfo`
+--
+ALTER TABLE `laptopinfo`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=160;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `safetybriefinginfo`
+--
+ALTER TABLE `safetybriefinginfo`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `vehicleinfo`
+--
+ALTER TABLE `vehicleinfo`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `visitorinfo`
+--
+ALTER TABLE `visitorinfo`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `visitrecord`
+--
+ALTER TABLE `visitrecord`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
